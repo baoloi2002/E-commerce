c902cb5c9dacf4dabdbf562a5375a4f24cea56f0
Tran Bao Loi <59519701+baoloi2002@users.noreply.github.com>
2024-03-31 03:16:03 +0700

    feat: core urls

--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request):
-    return HttpResponse("Welcome to the E-commerce website!")
+    return render(request, "core/index.html")
