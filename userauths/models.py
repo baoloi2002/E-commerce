c02f778ed132ac9c65642c48889ac704e9d52971
Tran Bao Loi <59519701+baoloi2002@users.noreply.github.com>
2024-03-31 20:03:11 +0700

    update: admin panel

--- userauths/models.py
+++ userauths/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     email = models.EmailField(unique=True)
     username = models.CharField(max_length=100, unique=True)
+    bio = models.CharField(max_length=255, blank=True, null=True)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["username"]
