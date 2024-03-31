05ccc8e5d8d8f42c047851b4f180754a77c3afa4
Tran Bao Loi <59519701+baoloi2002@users.noreply.github.com>
2024-03-31 23:02:01 +0700

    feat: sign-in

--- userauths/views.py
+++ userauths/views.py
@@ -3,6 +3,9 @@ from userauths.forms import UserRegistrationForm
 from django.contrib import messages
 from django.shortcuts import redirect
 from django.contrib.auth import login, authenticate
+from django.conf import settings
+
+User = settings.AUTH_USER_MODEL
 
 
 def register_view(request):
@@ -24,3 +27,29 @@ def register_view(request):
     context = {"form": form}
 
     return render(request, "userauths/sign-up.html", context)
+
+
+def login_view(request):
+    if request.user.is_authenticated:
+        return redirect("core:index")
+
+    if request.method == "POST":
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        user = authenticate(username=email, password=password)
+
+        try:
+            user = User.objects.get(email=email)
+        except:
+            messages.error(request, "User with {email} does not exist")
+
+        user = authenticate(request, username=email, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Login successful")
+            return redirect("core:index")
+        else:
+            messages.warning(request, "Invalid credentials")
+
+    context = {}
+    return render(request, "userauths/sign-in.html", context)
