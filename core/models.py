254130466c6b65c89cac36dd7cf212bf868a76ed
Tran Bao Loi <59519701+baoloi2002@users.noreply.github.com>
2024-04-02 15:00:51 +0700

    feat: proudct-list page

--- core/models.py
+++ core/models.py
@@ -90,6 +90,7 @@ class Product(models.Model):
 
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    vendor = models.ForeignKey(Vendor, on_delete=models.SET_NULL, null=True)
 
     title = models.CharField(max_length=100, default="Laptop")
     image = models.ImageField(upload_to=user_directory_path, default="product.jpg")
@@ -131,8 +132,8 @@ class Product(models.Model):
         return self.title
 
     def get_percentage(self):
-        new_price = (self.price / self.old_price) * 100
-        return new_price
+        new_price = (1 - self.price / self.old_price) * 100
+        return round(new_price,0)
 
 
 class ProductImages(models.Model):
--- core/views.py
+++ core/views.py
@@ -1,7 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from core.models import (
+    Category,
+    Tags,
+    Vendor,
+    Product,
+    ProductImages,
+    CartOrder,
+    CartOrderItem,
+    ProductReview,
+    WishList,
+    Address,
+)
+
 
 def index(request):
-    context = {"user": request.user}
+
+    # products = Product.objects.all().order_by("-id")
+    products = Product.objects.filter(
+        featured=True, product_status="published"
+    ).order_by("-id")
+
+    context = {"user": request.user, "products": products}
     return render(request, "core/index.html", context)
+
+def product_list_view(request):
+    products = Product.objects.filter(product_status="published")
+    context = {"products": products}
+    return render(request, "core/product-list.html", context)
